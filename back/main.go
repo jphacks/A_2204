c386c11c247e4ee7ccca85ba6f6adff719e5ef7f
tsoushi <sosimds@gmail.com>
2022-10-18 22:50:17 +0900

    Auth0の環境構築完了

--- back/main.go
+++ back/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"dietApp/myMiddleware"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/auth0/go-jwt-middleware/v2/validator"
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
@@ -16,6 +19,24 @@ func main() {
 	}
 
 	e := echo.New()
+
+	api := e.Group("/api") // Auth0の認証が必要なエンドポイント
+
+	// CORSの設定
+	api.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		Skipper:      middleware.DefaultSkipper,
+		AllowOrigins: []string{os.Getenv("FRONT_URL")},
+		AllowHeaders: []string{echo.HeaderAuthorization, echo.HeaderOrigin, echo.HeaderContentType},
+	}))
+
+	// Auth0の認証ミドルウェア
+	api.Use(myMiddleware.Auth0)
+
+	// =========== 以下にapiのルーティングを記述 ==========
+	api.GET("/test", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, c.Get("claims").(*validator.ValidatedClaims).RegisteredClaims)
+	})
+	// ===================================================
 
 	// Hello Worldを返すエンドポイントの作成
 	e.GET("/", func(c echo.Context) error {
