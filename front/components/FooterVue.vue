239fb1247eeb2d6ab3cd23cc5c9e6704382323f5
Andyyyy64 <takuyarossi77@gmail.com>
2022-10-18 20:20:18 +0900

    create-top-page-mock

---
+++ front/components/FooterVue.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <v-footer :absolute="!fixed" app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
