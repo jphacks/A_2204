54373610a36a8c8ab905af9ff29f8858ce8cc3c3
Yuorei <yuorei71@gmail.com>
2022-10-20 00:47:09 +0900

    PKとAUTO_INCREMENTの追加

--- back/operateDb/db.go
+++ back/operateDb/db.go
@@ -11,26 +11,26 @@ var db *gorm.DB
 
 // データベースに入れるtable
 type user struct {
-	Id       int       `json:"id"`
+	Id       int       `json:"id" gorm:"primaryKey" gorm:"AUTO_INCREMENT"`
 	Auth0_id string    `json:"auth0_id"`
 	Height   float64   `json:"height"`
 	Birthday time.Time `json:"birthday"`
 }
 type user_meal struct {
-	Id      int       `json:"id"`
+	Id      int       `json:"id" gorm:"primaryKey" gorm:"AUTO_INCREMENT"`
 	User_id int       `json:"user_id"`
 	Name    string    `json:"name"`
 	Calorie int       `json:"calorie"`
 	At      time.Time `json:"at"`
 }
 type user_weight struct {
-	Id      int       `json:"id"`
+	Id      int       `json:"id" gorm:"primaryKey" gorm:"AUTO_INCREMENT"`
 	User_id int       `json:"user_id"`
 	Weight  float64   `json:"weight"`
 	At      time.Time `json:"data"`
 }
 type character struct {
-	User_id int       `json:"user_id"`
+	User_id int       `json:"user_id" gorm:"primaryKey" gorm:"AUTO_INCREMENT"`
 	Name    string    `json:"name"`
 	Level   int       `json:"level"`
 	Weight  float64   `json:"weight"`
