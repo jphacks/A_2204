d75ed7f7575bb0eda1d54c4b28b1bce513bdadb3
Yuorei <yuorei71@gmail.com>
2022-10-19 20:10:20 +0900

    gormの環境構築完了

--- back/go.mod
+++ back/go.mod
@@ -3,6 +3,10 @@ module dietApp
 go 1.18
 
 require (
+	github.com/go-sql-driver/mysql v1.5.0 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/jinzhu/gorm v1.9.16 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/labstack/echo/v4 v4.9.1 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
@@ -14,4 +18,5 @@ require (
 	golang.org/x/net v0.0.0-20211015210444-4f30a5c0130f // indirect
 	golang.org/x/sys v0.0.0-20211103235746-7861aae1554b // indirect
 	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324 // indirect
 )
---
+++ back/operateDb/db.go
@@ -0,0 +1,66 @@
+package operateDb
+
+import (
+	"time"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+var db *gorm.DB
+
+// データベースに入れるtable
+type user struct {
+	Id       int       `json:"id"`
+	Auth0_id string    `json:"auth0_id"`
+	Height   float64   `json:"height"`
+	Birthday time.Time `json:"birthday"`
+}
+type user_meal struct {
+	Id      int       `json:"id"`
+	User_id int       `json:"user_id"`
+	Name    string    `json:"name"`
+	Calorie int       `json:"calorie"`
+	At      time.Time `json:"at"`
+}
+type user_weight struct {
+	Id      int       `json:"id"`
+	User_id int       `json:"user_id"`
+	Weight  float64   `json:"weight"`
+	At      time.Time `json:"data"`
+}
+type character struct {
+	User_id int       `json:"user_id"`
+	Name    string    `json:"name"`
+	Level   int       `json:"level"`
+	Weight  float64   `json:"weight"`
+	At      time.Time `json:"at"`
+	Exp     int       `json:"exp"`
+}
+
+func Init() {
+	DBMS := "mysql"
+	USER := "root"
+	PASS := ""
+	PROTOCOL := "tcp(localhost:3306)"
+	DBNAME := "dietapp"
+
+	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME
+	//データベースを開ける
+	dbCon, err := gorm.Open(DBMS, CONNECT)
+	//ローカル変数のdbCOnをグローバル変数のdbに入れる
+	db = dbCon
+	//作成するtableの構造体を引数に入れてテーブルを作る
+	db.AutoMigrate(&user{}, &user_meal{}, &user_weight{}, &character{})
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+func GetConnect() *gorm.DB {
+	return db
+}
+
+func CloseDb() {
+	db.Close()
+}
