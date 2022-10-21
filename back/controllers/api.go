a4086fe7c7ada2b7a76d6322aff144736dfa1be7
Yuorei <yuorei71@gmail.com>
2022-10-21 15:28:58 +0900

    DBのdeleteを追加

--- back/controllers/api.go
+++ back/controllers/api.go
@@ -1,7 +1,9 @@
 package controllers
 
 import (
+	"dietApp/operateDb"
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 )
@@ -54,7 +56,22 @@ func POST_user_weights(c echo.Context) error {
 
 // DELETE /user/weights/:id
 func DELETE_user_weights_id(c echo.Context) error {
-	return c.JSON(http.StatusOK, SampleJSON{"Coming soon"})
+	id := c.Param("id")
+	var int_id int
+	//stringからintにキャスト
+	int_id, _ = strconv.Atoi(id)
+	//構造体を読み込む
+	u := new(operateDb.User_weight)
+	if err := c.Bind(u); err != nil {
+		return c.String(http.StatusBadRequest, "bad request")
+	}
+	u.Id = int_id
+	db := operateDb.GetConnect()
+	// まずは削除したいレコードの情報を埋める
+	db.First(&u)
+	// 完全にレコードを特定できる状態で削除を行う
+	db.Delete(&u)
+	return c.JSON(http.StatusOK, u)
 }
 
 // PUT /user/weights/:id
